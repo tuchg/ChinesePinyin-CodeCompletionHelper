b13cf174032a077a60fbab5c7fd605b6308dcef4
fromwheretowhere <fromwheretowhere.service@gmail.com>
2020-12-21 21:27:56 -0800

    Revert "#12 对测试进行部分中文化，添加断言；添加开发说明"
    This reverts commit 21a1ff4e0b655e15e5996fb85d2a1a505f8f880d.

--- src/test/kotlin/CompletionTests.kt
+++ src/test/kotlin/CompletionTests.kt
@@ -3,7 +3,6 @@ import com.github.tuchg.nonasciicodecompletionhelper.utils.toPinyin
 import com.intellij.testFramework.fixtures.CompletionAutoPopupTestCase
 import pansong291.simplepinyin.Pinyin
 import kotlin.system.measureTimeMillis
-import org.junit.Assert
 
 
 /**
@@ -23,31 +22,31 @@ class CompletionTests : CompletionAutoPopupTestCase() {
         //myFixture.testCompletionVariants("", "han")
     }
 
-    fun `test @补全核心正确性测试`() {
-        val 标识符 = "handle测试嗯"
-        val 输入 = "hceshi"
+    fun `test @补全核心正确性性测试`() {
+        val original = "handle测试嗯"
+        val input = "hceshi"
 
-        toPinyin(标识符, Pinyin.LOW_CASE).forEach { 拼音 ->
-            Assert.assertEquals(6, countContainsSomeChar(拼音, 输入))
+        toPinyin(original, Pinyin.LOW_CASE).forEach { pinyin ->
+            println("$input 于 $pinyin 计为：${countContainsSomeChar(pinyin, input)}")
         }
     }
 
     fun `test @笛卡尔积测试`() {
-        val 耗时 = measureTimeMillis {
+        val costTime = measureTimeMillis {
             repeat(100000) {
                 toPinyin("啊嗯哦家", Pinyin.FIRST_UP_CASE)
 //                        .forEach { println(it) }
             }
         }
-        println("耗时：$耗时 ms")
+        println("耗时：$costTime ms")
     }
 
     fun `test @计算字符串内包含需要的单个字符的数量`() {
-        val 耗时 = measureTimeMillis {
+        val costTime = measureTimeMillis {
             repeat(10000) {
                 countContainsSomeChar("chijkdffd", "cjk")
             }
         }
-        println("耗时：$耗时 ms")
+        println("耗时：$costTime ms")
     }
 }
