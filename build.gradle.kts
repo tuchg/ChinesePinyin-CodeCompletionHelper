aff784ec509336370f327983ac392f6b0a931797
tuchg <24775733+tuchg@users.noreply.github.com>
2020-12-22 09:23:31 +0800

    :construction: 去除冗余构建

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-import io.gitlab.arturbosch.detekt.Detekt
 import org.jetbrains.changelog.closure
 import org.jetbrains.changelog.markdownToHTML
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
@@ -7,20 +6,16 @@ plugins {
     // Java support
     id("java")
     // Kotlin support
-    id("org.jetbrains.kotlin.jvm") version "1.4.10"
+    id("org.jetbrains.kotlin.jvm") version "1.4.21"
     // gradle-intellij-plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
     id("org.jetbrains.intellij") version "0.6.5"
     // gradle-changelog-plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
     id("org.jetbrains.changelog") version "0.6.2"
-    // detekt linter - read more: https://detekt.github.io/detekt/gradle.html
-    id("io.gitlab.arturbosch.detekt") version "1.14.2"
-    // ktlint linter - read more: https://github.com/JLLeitschuh/ktlint-gradle
-    id("org.jlleitschuh.gradle.ktlint") version "9.4.1"
 }
 
 // Import variables from gradle.properties file
 val pluginGroup: String by project
-val pluginName: String by project
+val pluginName_: String by project
 val pluginVersion: String by project
 val pluginSinceBuild: String by project
 val pluginUntilBuild: String by project
@@ -29,47 +24,32 @@ val platformType: String by project
 val platformVersion: String by project
 val platformDownloadSources: String by project
 val platformPlugins: String by project
+val pluginVerifierIdeVersions: String by project
 
 // Configure project's dependencies
 repositories {
-    mavenLocal()
     mavenCentral()
+    mavenLocal()
     jcenter()
 }
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("reflect"))
-    // detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.10.0")
 }
 
 // Configure gradle-intellij-plugin plugin.
 // Read more: https://github.com/JetBrains/gradle-intellij-plugin
 intellij {
-    pluginName = pluginName
+    pluginName = pluginName_
     version = platformVersion
     type = platformType
     downloadSources = platformDownloadSources.toBoolean()
     updateSinceUntilBuild = true
-
 //  Plugin Dependencies:
 //  https://www.jetbrains.org/intellij/sdk/docs/basics/plugin_structure/plugin_dependencies.html
 // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
     setPlugins(*platformPlugins.split(',').map(String::trim).filter(String::isNotEmpty).toTypedArray())
 }
-
-
-// Configure detekt plugin.
-// Read more: https://detekt.github.io/detekt/kotlindsl.html
-// detekt {
-//    config = files("./detekt-config.yml")
-//    buildUponDefaultConfig = true
-//
-//    reports {
-//        html.enabled = false
-//        xml.enabled = false
-//        txt.enabled = false
-//    }
-// }
 
 tasks {
     // Set the compatibility versions to 1.8
@@ -83,10 +63,6 @@ tasks {
         }
     }
 
-    withType<Detekt> {
-        jvmTarget = "1.8"
-    }
-
     patchPluginXml {
         version(pluginVersion)
         sinceBuild(pluginSinceBuild)
@@ -97,9 +73,10 @@ tasks {
             File("./README.md").readText().lines().run {
                 subList(indexOf("<!-- Plugin description -->") + 1, indexOf("<!-- Plugin description end -->"))
             }.joinToString("\n").run {
-                val last = this.replaceFirst("<!-- E -->",
-                        // language=HTML
-                        """
+                val last = this.replaceFirst(
+                    "<!-- E -->",
+                    // language=HTML
+                    """
 <h3>English</h3>
 
 > The original intention is that for some businesses that are not suitable for expression in English, you can use a straightforward native language instead of pinyin, and then call what is expressed in your native language as you usually write code to solve some naming difficulties 🤔
@@ -115,7 +92,8 @@ Compatible with all the Intellij Platform product, indiscriminate Chinese progra
 > 为啥这里会有一段不符合插件受众的别扭英文介绍💬  <a href="https://github.com/tuchg/ChinesePinyin-CodeCompletionHelper/issues/5">#issues-5</a>
 
 <h3>中文</h3>
-""")
+"""
+                )
                 markdownToHTML(last)
             }
         })
@@ -124,6 +102,9 @@ Compatible with all the Intellij Platform product, indiscriminate Chinese progra
         changeNotes(closure {
             changelog.getLatest().toHTML()
         })
+    }
+    runPluginVerifier {
+        ideVersions(pluginVerifierIdeVersions)
     }
 
     publishPlugin {
