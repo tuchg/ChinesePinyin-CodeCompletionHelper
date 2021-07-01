4f800dc1726dd1ce5229ebd278b62f71a1fcd9e1
tuchg <24775733+tuchg@users.noreply.github.com>
2021-07-01 10:30:34 +0800

    :bug: 引入MinusculeMatcher用于解决大小写不敏感的匹配问题 [actions skip]

--- src/main/kotlin/com/github/tuchg/nonasciicodecompletionhelper/model/ChinesePrefixMatcher.kt
+++ src/main/kotlin/com/github/tuchg/nonasciicodecompletionhelper/model/ChinesePrefixMatcher.kt
@@ -2,18 +2,58 @@ package com.github.tuchg.nonasciicodecompletionhelper.model
 
 import com.github.tuchg.nonasciicodecompletionhelper.utils.countContainsSomeChar
 import com.github.tuchg.nonasciicodecompletionhelper.utils.toPinyin
+import com.intellij.codeInsight.CodeInsightSettings
 import com.intellij.codeInsight.completion.PlainPrefixMatcher
+import com.intellij.codeInsight.completion.impl.CamelHumpMatcher
+import com.intellij.psi.codeStyle.MinusculeMatcher
+import com.intellij.psi.codeStyle.NameUtil
 import pansong291.simplepinyin.Pinyin
 
 /**
+ * 转换中文使之与IDE内提取标识符进行有效识别的关键类
  * @author: tuchg
  * @date: 2021/2/4 13:58
- * @description: 转换中文使之与IDE沟通的关键类
  */
-class ChinesePrefixMatcher(prefix: String) : PlainPrefixMatcher(prefix) {
+class ChinesePrefixMatcher : PlainPrefixMatcher {
+    private var myMatcher: MinusculeMatcher
+    private var myCaseSensitive = false
+    private var myTypoTolerant = false
+
+    companion object {
+//        val log = getLogger(ChinesePrefixMatcher::class)
+    }
+
+    constructor(prefix: String) : this(prefix, false, true)
+
+    constructor(prefix: String, caseSensitive: Boolean, typoTolerant: Boolean) : super(prefix) {
+        myCaseSensitive = caseSensitive
+        myTypoTolerant = typoTolerant
+        myMatcher = createMatcher(myCaseSensitive)
+    }
+
+    /**
+     * copied from CamelHumpMathcer
+     * 创建MinusculeMatcher 用于字符模式匹配
+     */
+    private fun createMatcher(caseSensitive: Boolean): MinusculeMatcher {
+        val prefix = CamelHumpMatcher.applyMiddleMatching(myPrefix)
+        var builder = NameUtil.buildMatcher(prefix)
+        if (caseSensitive) {
+            val setting = CodeInsightSettings.getInstance().completionCaseSensitive
+            if (setting == CodeInsightSettings.FIRST_LETTER) {
+                builder = builder.withCaseSensitivity(NameUtil.MatchingCaseSensitivity.FIRST_LETTER)
+            } else if (setting == CodeInsightSettings.ALL) {
+                builder = builder.withCaseSensitivity(NameUtil.MatchingCaseSensitivity.ALL)
+            }
+        }
+        if (myTypoTolerant) {
+            builder = builder.typoTolerant()
+        }
+        return builder.build()
+    }
 
     override fun prefixMatches(name: String): Boolean {
-
+//        log.info { "${prefix} - ${name} ${myMatcher.matches(name)}" }
         return if (Pinyin.hasChinese(name)) {
             for (s in toPinyin(name, Pinyin.LOW_CASE)) {
                 if (countContainsSomeChar(s, prefix) >= prefix.length) {
@@ -21,7 +61,7 @@ class ChinesePrefixMatcher(prefix: String) : PlainPrefixMatcher(prefix) {
                 }
             }
             return false
-        } else super.prefixMatches(name)
+        } else myMatcher.matches(name)
     }
 
 
