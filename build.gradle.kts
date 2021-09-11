f911664d91475b87ade34c1d061b381d0a8ad4e0
tuchg <24775733+tuchg@users.noreply.github.com>
2021-09-11 14:40:43 +0800

    :rotating_light: 修复 Java编译时的GBK错误问题 [actions skip]

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,6 +58,7 @@ tasks {
     withType<JavaCompile> {
         sourceCompatibility = "1.8"
         targetCompatibility = "1.8"
+        options.encoding = "UTF-8"
     }
     withType<KotlinCompile> {
         kotlinOptions.jvmTarget = "1.8"
